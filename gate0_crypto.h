d70eb07648ffb0dc9fc98a6dba3928aa7a9ed152
Robbie Gibbons <robbiegibbons60@gmail.com>
2026-09-03 05:59:59 -0700

    Complete gate0_crypto.h: all function declarations

--- gate0_crypto.h
+++ gate0_crypto.h
@@ -39,4 +39,16 @@ uint64_t timestamp_us;
 uint8_t entry_hash[32];
 uint8_t chain_hash[32];
 } gate0_chain_link_t;
+gate0_status_t gate0_ed25519_keygen(gate0_ed25519_keypair_t *kp);
+gate0_status_t gate0_ed25519_sign(const gate0_ed25519_keypair_t *kp,const uint8_t *msg,size_t len,uint8_t sig[64]);
+gate0_status_t gate0_ed25519_verify(const uint8_t pub[32],const uint8_t *msg,size_t len,const uint8_t sig[64]);
+gate0_status_t gate0_x25519_keygen(gate0_x25519_keypair_t *kp);
+gate0_status_t gate0_x25519_dh(const gate0_x25519_keypair_t *local,const uint8_t remote[32],uint8_t secret[32]);
+gate0_status_t gate0_aead_init(gate0_aead_ctx_t *ctx,const uint8_t key[32]);
+gate0_status_t gate0_aead_encrypt(gate0_aead_ctx_t *ctx,const uint8_t *pt,size_t pt_len,uint8_t *ct,uint8_t tag[16],uint8_t nonce[12]);
+gate0_status_t gate0_aead_decrypt(gate0_aead_ctx_t *ctx,const uint8_t *ct,size_t ct_len,const uint8_t tag[16],const uint8_t nonce[12],uint8_t *pt);
+gate0_status_t gate0_sha256(const uint8_t *data,size_t len,uint8_t digest[32]);
+gate0_status_t gate0_chain_init(gate0_chain_link_t *link,const uint8_t *entry,size_t len);
+gate0_status_t gate0_chain_append(gate0_chain_link_t *prev,gate0_chain_link_t *next,const uint8_t *entry,size_t len);
+gate0_status_t gate0_chain_verify(const gate0_chain_link_t *prev,const gate0_chain_link_t *next);
 #endif
