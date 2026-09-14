111a81696ca600548afdf9a6806a6c0fcb5d5c14
Robbie Gibbons <robbiegibbons60@gmail.com>
2026-09-14 10:48:27 -0700

    Add hazard pointer slots and acquire/release inlines to MTKState

--- mtk.h
+++ mtk.h
@@ -2,6 +2,7 @@
 #define MTK_H
 #include <stdint.h>
 #include <sodium.h>
+#include <stdatomic.h>
 #define MTK_KEY_LEN 32
 #define MTK_SEED_LEN 32
 typedef struct {
@@ -11,7 +12,16 @@ typedef struct {
     uint64_t last_rotation_ns;
     uint32_t access_attempts;
     uint32_t legitimate_accesses;
+    _Atomic uintptr_t hazard[8];
 } MTKState;
+
+#define MTK_MAX_READERS 8
+static inline void mtk_hazard_acquire(MTKState *m, int slot, uintptr_t ptr) {
+    atomic_store_explicit(&m->hazard[slot], ptr, memory_order_seq_cst);
+}
+static inline void mtk_hazard_release(MTKState *m, int slot) {
+    atomic_store_explicit(&m->hazard[slot], 0, memory_order_release);
+}
 void mtk_init(MTKState *m, const uint8_t *seed);
 void mtk_rotate(MTKState *m);
 int  mtk_verify(MTKState *m, const uint8_t *key);
